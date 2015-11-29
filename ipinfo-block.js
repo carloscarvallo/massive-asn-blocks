35de6a40f51531a557237b0e9018cbff3697eef4
Carlos Carvallo <erotomania669@gmail.com>
2015-11-29 05:09:07 -0300

    add user ip

--- ipinfo-block.js
+++ ipinfo-block.js
@@ -5,40 +5,47 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 
+rl.write("===============================\n");
 rl.write("Bloques de IPs a partir del ASN\n");
-rl.question("Ingrese la IP para referencia: ", function(reqip){
+rl.write("===============================\n\n");
 
-  jsonHttp.getJson('http://ipinfo.io/'+reqip+'/json', function(err, response){
+jsonHttp.getJson('http://ipinfo.io/json', function (err, res){
+  rl.write("Su ip actual es: "+ res.ip +"\n\n");
 
-    var res = response.org, pos = res.indexOf(" "), asn = res.substring(0, pos);
+  rl.question("Ingrese IP de referencia: ", function(reqip){
 
-    var pageToVisit = "http://4.ipinfo.io/"+asn;
+    jsonHttp.getJson('http://ipinfo.io/'+reqip+'/json', function(err, response){
 
-    request(pageToVisit, function(error, response, body) {
-       if(error) {
-         console.log("Error: " + error);
-       }
-       if(response.statusCode === 200) {
-         var $ = cheerio.load(body), ip = [];
+      var res = response.org, pos = res.indexOf(" "), asn = res.substring(0, pos);
 
-         $('tr td a').each(function(i, elem){
-           ip[i] = $(this).text();
-         });
-         for(var i = 1, newip = []; i < ip.length - 1; i++){
-           if( ip[i] === "" ){
-             ip.splice(i, 1);
-           }
-           newip.push({bloque : ip[i]});
+      var pageToVisit = "http://4.ipinfo.io/"+asn;
+
+      request(pageToVisit, function(error, response, body) {
+         if(error) {
+           console.log("Error: " + error);
          }
-         console.log(newip);
-      }
-      fs.writeFile( asn + "-" + ip[0] + '.txt', JSON.stringify(newip), function(err) {
-      if (err)
-        console.log(err);
-      else
-        console.log('Operacion completada');
+         if(response.statusCode === 200) {
+           var $ = cheerio.load(body), ip = [];
+
+           $('tr td a').each(function(i, elem){
+             ip[i] = $(this).text();
+           });
+           for(var i = 1, newip = []; i < ip.length - 1; i++){
+             if( ip[i] === "" ){
+               ip.splice(i, 1);
+             }
+             newip.push(ip[i]);
+           }
+           console.log(newip);
+        }
+        fs.writeFile( asn + "-" + ip[0] + '.txt', JSON.stringify(newip), function(err) {
+        if (err)
+          console.log(err);
+        else
+          console.log('Operacion completada');
+        });
       });
     });
+    rl.close();
   });
-  rl.close();
 });

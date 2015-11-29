637115cdbf7752b06fb5286f0d7c519ab19fc9da
Carlos Carvallo <erotomania669@gmail.com>
2015-11-29 03:19:27 -0300

    paso 3

--- cheerio-two.js
+++ cheerio-two.js
@@ -2,25 +2,39 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 
-var pageToVisit = "http://4.ipinfo.io/AS23201";
-request(pageToVisit, function(error, response, body) {
-   if(error) {
-     console.log("Error: " + error);
-   }
-   if(response.statusCode === 200) {
-     var $ = cheerio.load(body);
-     var ip = [];
-     //console.log($('tr td a').text());
-     $('tr td a').each(function(i, elem){
-       ip[i] = $(this).text();
-     });
-     //ip.join(",");
-     for(var i = 1, newip = []; i < ip.length - 1; i++){
-       if( ip[i] === "" ){
-         ip.splice(i, 1);
-       }
-       newip[i -1] = ip[i];
+var jsonHttp = require('json-http');
+var fs = require('fs');
+
+jsonHttp.getJson('http://ipinfo.io/json', function(err, response){
+
+  var res = response.org, pos = res.indexOf(" "), asn = res.substring(0, pos);
+
+  var pageToVisit = "http://4.ipinfo.io/"+asn;
+
+  request(pageToVisit, function(error, response, body) {
+     if(error) {
+       console.log("Error: " + error);
      }
-     console.log(newip);
-  }
+     if(response.statusCode === 200) {
+       var $ = cheerio.load(body);
+       var ip = [];
+       $('tr td a').each(function(i, elem){
+         ip[i] = $(this).text();
+       });
+       for(var i = 1, newip = []; i < ip.length - 1; i++){
+         if( ip[i] === "" ){
+           ip.splice(i, 1);
+         }
+         newip[i -1] = ip[i];
+       }
+       console.log(newip);
+    }
+  });
+
+  //fs.writeFile( asn +'.txt', asn, function(err) {
+  //if (err)
+  //  console.log(err);
+  //else
+  //  console.log('Operacion completada');
+  //});
 });

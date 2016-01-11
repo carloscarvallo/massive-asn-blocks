2d8e77af768e806ab3b16a8249f046d52605e565
Carlos Carvallo <erotomania669@gmail.com>
2016-01-11 20:38:23 -0300

    log command executed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log("==================\n");
 asn.init(function (page) {
   blocks.scrap(page, function (data) {
     for (var i = 0; i < data.length; i++) {
-      var netblock = data[i][0], numIPs = data[i][1], args = [];
+      var netblock = data[i][0], numIPs = data[i][1], args = [], com = "";
       console.log("\n");
       console.log(clc.blue(data[i][0]+"/"+data[i][1]));
       for (var i = 3; i < proc.length; i++) {
@@ -19,7 +19,15 @@ asn.init(function (page) {
       }
       args.push(netblock+"/"+numIPs);
       console.log("command executed");
-      console.log(proc[2]+" "+args);
+      for (var i = 3; i < proc.length; i++) {
+        if (i > 3) {
+          com += " "+proc[i];
+        } else {
+          com += proc[i];
+        }
+      }
+      console.log(proc[2], com+" "+netblock+"/"+numIPs);
+      console.log("\n");
       spawnSync(proc[2], args, {stdio:[0,1,2]});
     }
   });

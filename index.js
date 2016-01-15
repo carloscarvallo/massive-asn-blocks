90f4bcd746668a629af7bdcec91991b2d1e70aa1
Carlos Carvallo <erotomania669@gmail.com>
2016-01-15 02:47:43 -0300

    details

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ clc = require('cli-color'),
 spawnSync = require('child_process').spawnSync,
 proc = process.argv;
 
-console.log("==================");
+console.log("\n==================");
 console.log("Massive ASN blocks");
 console.log("==================\n");
 
@@ -13,12 +13,13 @@ asn.init(function (page) {
     for (var i = 0; i < data.length; i++) {
       var netblock = data[i][0], numIPs = data[i][1], args = [], com = "";
       console.log("\n");
-      console.log(clc.blue(data[i][0]+"/"+data[i][1]));
       for (var i = 3; i < proc.length; i++) {
         args.push(proc[i]);
       }
       args.push(netblock+"/"+numIPs);
-      console.log("command executed");
+      console.log("++++++++++++++++");
+      console.log("Command executed");
+      console.log("++++++++++++++++\n");
       for (var i = 3; i < proc.length; i++) {
         if (i > 3) {
           com += " "+proc[i];
@@ -26,8 +27,8 @@ asn.init(function (page) {
           com += proc[i];
         }
       }
-      console.log(proc[2], com+" "+netblock+"/"+numIPs);
-      console.log("\n");
+      if (proc.length == 3) { console.log(clc.blue(proc[2], netblock+"/"+numIPs)+"\n"); }
+      else { console.log(clc.blue(proc[2], com+" "+netblock+"/"+numIPs)+"\n"); }
       spawnSync(proc[2], args, {stdio:[0,1,2]});
     }
   });
